c8f607bf614cb2ae7845eb4b2cac2bbf60d15ee0
compalex <47093016+compalex@users.noreply.github.com>
2019-01-29 13:54:50 -0500

    Add files via upload

--- Task1.java
+++ Task1.java
@@ -1,36 +1,36 @@
 package myTasks;
 
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class Task1 {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-		int inputNumber = getInput();
+        int inputNumber = getInput();
 
-		for (int i = 0; i <= inputNumber; i++) {
-			if (isPrime(i)) {
-				System.out.print(i + " ");
-			}
-		}
-	}
+        for (int i = 0; i <= inputNumber; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
 
-	static int getInput() {
-		Scanner sc = new Scanner(System.in);
-		int number = sc.nextInt();
-		return number;
-	}
+    static int getInput() {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        return number;
+    }
 
-	static boolean isPrime(int currentNumber) {
-		if (currentNumber < 2) {
-			return false;
-		}
+    static boolean isPrime(int currentNumber) {
+        if (currentNumber < 2) {
+            return false;
+        }
 
-		for (int i = 2; i < currentNumber; i++) {
-			if (currentNumber % i == 0) {
-				return false;
-			}
-		}
-		return true;
-	}
+        for (int i = 2; i < currentNumber; i++) {
+            if (currentNumber % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
