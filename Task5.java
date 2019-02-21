d15b78a219a480bad19b75b8de6ae8bca1246416
compalex <alexander.ermak95@gmail.com>
2019-02-21 00:02:11 -0500

    added Task5 which is superhot

---
+++ Task5.java
@@ -0,0 +1,35 @@
+package myTasks;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Task5 {
+
+    public static void main(String[] args) {             
+        System.out.println("Is word a palindrome? --> " + isItPalindrome(getWord()));
+    }
+
+    private static String getWord() {
+        Scanner sc = new Scanner(System.in);       
+        try {
+            System.out.println("Input word:");
+            return sc.nextLine();
+        } catch (InputMismatchException e) {
+            System.out.println("Error, input an integer number.");
+            return sc.nextLine();
+        } finally {
+            sc.close();
+        }      
+    }
+
+    private static boolean isItPalindrome(String word) {
+        char[] wordArr = word.toCharArray();
+        
+        for(int i = 0; i<wordArr.length/2; i++) {
+            if(wordArr[i] != wordArr[wordArr.length-1-i]) {
+                return false;
+            } 
+        }
+        return true;
+    }
+}
