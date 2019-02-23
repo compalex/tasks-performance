98223d584843166f6cdca229b1f7cf1f7ecd737b
compalex <alexander.ermak95@gmail.com>
2019-02-22 23:43:44 -0500

    changed the package

---
+++ myTasks/src/myTasks/Task2.java
@@ -0,0 +1,44 @@
+package myTasks;
+
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+/**
+ * Search for fibonacci numbers
+ * @version 1.0 
+ * @author compalex
+ */
+public class Task2 {
+    private static final String INPUT_PROMPT = "Input an integer number:"; 
+    private static final String INPUT_ERROR = "Error! Wrong input! "; 
+
+    public static void main(String[] args) {
+        printFibo(getInput());        
+    }
+
+    private static int getInput() {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println(INPUT_PROMPT);
+            return sc.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.print(INPUT_ERROR);
+            return getInput();
+        } finally {
+            sc.close();
+        }
+    }
+
+    private static void printFibo(int inputNumber) {
+        int numBefore = 0;
+        
+        for (int i = 1; numBefore <= inputNumber; i += numBefore) {
+            if (numBefore != 0) {           //'cuz 0 doesn't belong to fibo nums
+                System.out.print (numBefore + " ");
+            }
+            numBefore = i - numBefore;
+        }
+    }
+
+}
